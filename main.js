b08f8e87f86fad8f4b915175ed88c5783394b823
Aqeel <sajjad.masood@codenterprise.com>
2018-02-23 11:43:09 +0500

    Implemented update method

--- main.js
+++ main.js
@@ -4,15 +4,20 @@ class LangVar {
     * @param select the HTML select format default is [lv]
     */
     constructor(obj = {}, selector = '') {
-       this.obj = obj;
-       this.selector = selector + "lv";
+       const vars = {
+           obj : obj, // object for direct variable
+           selector: selector + "lv",  // query selector
+           modules: {} // modules list with key > name & v > innerContent
+       }
+       Object.assign(this, vars);
        if (this.obj) {
            this._extract();
        }
 
        // return accessible functions and variables for class instance
        return {
-           module: this.module.bind(this)
+           module: this.module.bind(this),
+           update: this.update.bind(this)
         };
     }
 
@@ -23,16 +28,43 @@ class LangVar {
     * @param obj the object of the variables to write
     */
     module(n, obj = {}) {
-        const m = document.querySelector("["+ this.selector +"-module="+ n +"]"); // getting the module
-        let m_c = m.innerHTML;
+        const m = document.querySelector("[" + this.selector + "-module=" + n + "]"); // getting the module container
+        let m_c = null;  // store updated content to return
+        if (!this.modules[n]) {
+            this.modules[n] = m.innerHTML; // adding module to the modules list
+        }
         try {
-            Object.keys(obj).map(function (k) {
-                m_c = m_c.replace("%"+ k +"%", obj[k]);
-            });
+            m_c = this._putContentOnModule(obj, this.modules[n]); // get updated module by replacing variables
         } catch (error) {
-            console.log(error);
+            console.warn("Unable to update module "+ n);
         }
         m.innerHTML = m_c;
+        return this;
+    }
+
+
+    /* 
+     * @type public to update existing variables or module
+     * @param g if object then update vars else update the module
+     * @param obj the object of the variables to update
+     */
+    update(g, obj) {
+        const self = this;
+        if (typeof g === "object") {
+            Object.keys(g).map(function (k) {
+                try {
+                    // updating with dirty checking methodology
+                    if (g[k] !== self.obj[k]) {
+                        self._putContent(k, g[k]); // if direct variable then update
+                    }
+                } catch (error) {
+                    console.warn("Unable to find variable [" + k + "] to update");
+                }
+            });
+            this.obj = Object.assign(this.obj, g);
+        } else {
+            self.module(g, obj); // if module defined then update module
+        }
     }
 
 
@@ -66,4 +98,48 @@ class LangVar {
             c[0].innerHTML = v;
         }
     }
+
+
+    /* 
+     * @type Private Function to write down variable in module()
+     * @param obj object to replace in content
+     * @param e content to replace
+     */
+    _putContentOnModule(obj, e) {
+        Object.keys(obj).map(function (k) {
+            e = e.replace("%" + k + "%", obj[k]);
+        });
+        return e;
+    }
+}
+
+
+/* 
+* @type function to compare two objects
+* @param a,b object for comparison
+*/
+function isEquivalent(a, b) {
+    // Create arrays of property names
+    var aProps = Object.getOwnPropertyNames(a);
+    var bProps = Object.getOwnPropertyNames(b);
+
+    // If number of properties is different,
+    // objects are not equivalent
+    if (aProps.length != bProps.length) {
+        return false;
+    }
+
+    for (var i = 0; i < aProps.length; i++) {
+        var propName = aProps[i];
+
+        // If values of same property are not equal,
+        // objects are not equivalent
+        if (a[propName] !== b[propName]) {
+            return false;
+        }
+    }
+
+    // If we made it this far, objects
+    // are considered equivalent
+    return true;
 }
